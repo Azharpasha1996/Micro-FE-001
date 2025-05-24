df799422c777d5fb6cfcb4c6ff6d4fe8a628f5ea
azhar <azharpasha791@gmail.com>
2025-05-24 12:51:26 +0530

    stage-III@12:51

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from "react";
 import { BehaviorSubject } from "rxjs";
+
 
 const API_SERVER = "http://localhost:8080";
 
@@ -21,5 +23,16 @@ export const login = (username, password) =>
         // getCart();
         return data.access_token;
       });
+    
+export function useLoggedIn() {
+    const [loggedIn, setLoggedIn] = useState(!!jwt.value);
+    useEffect(() => {
+        setLoggedIn(!!jwt.value);
+        return jwt.subscribe((c) => {
+            setLoggedIn(!!jwt.value);
+        });
+    }, []);
+    return loggedIn;
+}
   
   
